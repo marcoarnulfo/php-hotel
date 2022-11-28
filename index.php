5b8f4f740feda31b8ed9d4eaa0928e013759771e
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-11-28 14:32:16 +0100

    update ex

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+
+
+
+/*
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale. Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
+    <title>Hotel</title>
+</head>
+
+<body>
+
+    <table>
+        <?php foreach($hotels as $hotel):?>
+        <tr>
+            <td><?= $hotel['name']; ?></td>
+            <td><?= str_replace("Descrizione", "1", $hotel['description'])?></td>
+            <td><?= $hotel['parking']; ?></td>
+            <td><?= $hotel['vote']; ?></td>
+            <td><?= $hotel['distance_to_center']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($hotels as $hotel): ?>
+            <tr>
+                <td scope="row"><?= $hotel['name']; ?></td>
+
+                <td><?= $hotel['description']; ?></td>
+                <td><?= $hotel['parking']; ?></td>
+                <td><?= $hotel['vote']; ?></td>
+                <td><?= $hotel['distance_to_center']; ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
